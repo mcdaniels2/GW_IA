2bc2db86f53ae1e8cfc5fbf845efe8b3b8bc3ac9
Molly <mcdaniels2@wisc.edu>
2019-12-21 07:49:33 -0500

    added files to R, ran prelim analyses
    added all .bib files from this comp & lab comp to R, converted, & ran initial analyses. Waiting for .bib files from Emily to finish

--- scripts/upload_bib_files.R
+++ scripts/upload_bib_files.R
@@ -19,11 +19,142 @@ library(tidyverse)
 ##### load GCC list data #####
 
 # pull file from directory:
-gcclist <- read.csv("/Users/Amber/Documents/GitHub/GW_IA/originaldata/DNR_GW_AllProjects19852018.csv", header=TRUE, na.strings="")
+gcclist <- read.csv("/Users/Amber/Documents/GitHub/GW_IA/originaldata/DNR_GW_AllProjects19852018.csv", 
+                    header=TRUE, na.strings="")
+investigatorstatus <- read.csv("/Users/Amber/Documents/GitHub/GW_IA/originaldata/investigators_complete_122019.csv", 
+                               header=TRUE, na.strings="") # investigators_complete_122019 is the non-duplicate list ofinvestigators with their search statuses
+View(investigatorstatus)
 
 ###
 
-##### load bib files #####
+##### load bib files (12/20/19) #####
+
+lapply(files, readFiles)
+
+bibs <- readFiles("/Users/Amber/Documents/GitHub/GW_IA/authorsgs/babiarzc.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/bannermanr.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/barmanm.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/bartschl.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/bowmang.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/bowserc.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/boyle.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/bradburyk_prelim.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/bravoh.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/bryek.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/cardiffm.bib",  
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/clancyk.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/clarya.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/diebelm.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/dodsons.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/dunningc.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/eggerts.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/evangelistaf.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/evansc.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/gaffields.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/geissingerp.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/gindervogelm.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/gotkowitzm.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/grotek.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/gulecs.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/hedmanc.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/hemmingj.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/hooyert.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/huntr.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/johnsont.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/karthikeyank.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/kleinheinzg.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/kluenders.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/knobelochl.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/kucharikc.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/kussoww.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/lacossec.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/larsonr.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/lathropr.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/lepaind.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/leporeb.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/lij.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/lil.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/liz.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/loheides.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/luczajj.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/macwilliamsm.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/maleckik.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/markhaml.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/masarikk.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/mcdonaldc.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/mcginleyp.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/mcintirem.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/mcintyrep.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/mcmahonk.bib",
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/nitkaa.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/normanj.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/oconnork.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/olstadtj.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/pageg.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/parentl.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/parsenm.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/pedersenj.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/rabinowitzh.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/remucalc.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/roott.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/ruarkm.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/schauerj.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/schreiberm.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/scottj.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/shaferm.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/shethn.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/skalbeckj.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/sorsak.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/stelzerr.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/steuerj.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/stoorr.bib",
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/swansons.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/tangc.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/underwoodc.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/wuc.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/xus.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/zhangh.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/zornm.bib", 
+                  "/Users/Amber/Documents/GitHub/GW_IA/authorsgs/zwolinskim.bib")
+View(bibs)
+
+bibsd <- convert2df(bibs, dbsource = "isi", format = "bibtex")
+head(bibsd)
+colnames(bibsd)
+
+#AU		Authors
+#TI		Document Title
+#SO		Publication Name (or Source)
+#JI		ISO Source Abbreviation
+#DT		Document Type
+#DE		Authors' Keywords
+#ID		Keywords associated by SCOPUS or WoS database
+#AB		Abstract
+#C1		Author Address
+#RP		Reprint Address
+#CR		Cited References
+#TC		Times Cited
+#PY		Year
+#SC		Subject Category
+#UT		Unique Article Identifier
+#DB		Database
+
+summary(bibsd)
+
+###
+
+##### analysis of "bibs" articles #####
+
+results <- biblioAnalysis(bibsd, sep = ";")
+options(width=100)
+S <- summary(object = results, k = 10, pause = FALSE)
+
+plot(x = results, k = 10, pause = FALSE)
+
+
+###
+
+##### OLD - load bib files #####
 
 # pull files from directory:
 bradburys <- readFiles("/Users/Amber/Documents/GitHub/GW_IA/authorsc/bradbury.bib")
